1cbf7c0c990189ff4d0f81ce88219d79b86f5f20
Savan-H <savanhathalia@outlook.com>
2024-07-26 16:45:42 +0100

    Graphics pipeline overview

---
+++ 1 - Vulkan Start/lve_pipeline.cpp
@@ -0,0 +1,39 @@
+#include "lve_pipeline.h"
+
+#include <fstream> // for input file stream object
+#include <stdexcept>
+#include <iostream>
+
+namespace lve {
+	LvePipeline::LvePipeline(const std::string& vertFilePath, const std::string& fragFilepath)
+	{
+		createGraphicsPipeline(vertFilePath, fragFilepath);
+	}
+	std::vector<char> lve::LvePipeline::readFile(const std::string& filepath)
+	{
+		std::ifstream file(filepath, std::ios::ate | std::ios::binary); // ate means when file is open, seeks the end immediately. binary means read in binary to avoid any text transformations.
+		
+		if (!file.is_open()) {
+			throw std::runtime_error("Failed to open file: " + filepath);
+		}
+
+		size_t fileSize = static_cast<size_t>(file.tellg()); // ate lets us get the file size right away since we are at the end
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0); // seek to start
+		file.read(buffer.data(), fileSize); // read into buffer
+
+		file.close();
+		return buffer;
+	}
+
+	void LvePipeline::createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilepath)
+	{
+		auto vertCode = readFile(vertFilePath);
+		auto fragCode = readFile(fragFilepath);
+
+		std::cout << "Vertex Shader Code Size: " << vertCode.size() << std::endl;
+		std::cout << "Fragment Shader Code Size: " << fragCode.size() << std::endl;
+	}
+
+}
